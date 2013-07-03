377230bf00376e6d4318bd6d0fb130195a277ad3
John C. Dages <jdages@woh.rr.com>
2013-07-03 12:03:13 -0400

    Programming exercise for John

---
+++ ACLens.Programming.Examination.Senior/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACLens.Programming.Examination.Senior
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
---
+++ ACLens.Programming.Examination.Senior/Sorting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACLens.Programming.Examination.Senior
+{
+    interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetLongestServingEmployee(IEnumerable<Employee> employees)
+    }
+   public class EmployeeService : IEmployeeService
+    {
+    public IEnumerable<Employee> GetAllEmployees()
+{
+ 	throw new NotImplementedException();
+}
+
+public Employee GetLongestServingEmployee(IEnumerable<Employee> employees)
+{
+ 	throw new NotImplementedException();
+}
+}
+}
